36f76abd53b59c2f44f7053c0dde2bc18069ef1c
tejas kahirnar <tejaskhairnar256@gmail.com>
2025-09-02 16:47:51 +0530

    add docutor count in count controller

--- controller/products/countController.js
+++ controller/products/countController.js
@@ -1,5 +1,6 @@
 import Order from "../../models/Order.js";
 import Product from "../../models/Product.js";
+import Doctor from "../../models/Doctor.js";
 
 
 export const getUserStats = async (req, res) => {
@@ -12,9 +13,13 @@ export const getUserStats = async (req, res) => {
     // Count total products in DB
     const productsCount = await Product.countDocuments();
 
+    // count total doctors in db
+    const doctorsCount = await Doctor.countDocuments();
+
     res.json({
       ordersCount,
       productsCount,
+      doctorsCount
     });
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err.message });
