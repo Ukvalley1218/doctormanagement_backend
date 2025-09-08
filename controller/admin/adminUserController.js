b03464600437f824f9a15cf193e5c9e984f45d63
tejas kahirnar <tejaskhairnar256@gmail.com>
2025-09-08 14:51:02 +0530

    createuser api changes

--- controller/admin/adminUserController.js
+++ controller/admin/adminUserController.js
@@ -18,7 +18,7 @@ export const createUser = async (req,res)=>{
     res.status(201).json({ message: "User created successfully", user });
     
   } catch (error) {
-    res.status(500).json({ message: "Server error", error: err.message });
+    res.status(500).json({ message: "Server error", error: error.message });
   }
 }
 
