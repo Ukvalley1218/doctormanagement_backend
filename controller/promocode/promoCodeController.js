eec8421d40f1b96a0c04f49a10a6ab417bfcaa38
tejas kahirnar <tejaskhairnar256@gmail.com>
2025-09-17 16:44:17 +0530

    add proper error in createpromo code

--- controller/promocode/promoCodeController.js
+++ controller/promocode/promoCodeController.js
@@ -8,6 +8,7 @@ export const createPromoCode = async (req, res) => {
     if (!code || !discountPercentage || !expiryDate || !usageLimit) {
       return res.status(400).json({ message: "Invalid data" });
     }
+    if(code) return res.status(400).json({ message: "Promo code already exists" });
     const promo = new Promocode({
       code,
       discountPercentage,
