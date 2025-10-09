6a837198c0071f8f19bab509bc091b51b365692c
Tejaskhairnar7 <144803437+Tejaskhairnar7@users.noreply.github.com>
2025-10-09 13:28:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,9 @@ app.use(
 
 
 // middleware
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 // Routes
 app.use("/api/auth", authRoutes);
