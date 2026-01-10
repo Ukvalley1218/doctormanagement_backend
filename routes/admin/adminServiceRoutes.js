9faf698852dfd2b9974a2ae8e816606e6b718b18
tejas kahirnar <tejaskhairnar256@gmail.com>
2026-01-10 16:34:22 +0530

    create services and packages module

--- routes/admin/adminServiceRoutes.js
+++ routes/admin/adminServiceRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createService ,updateService,deleteService,toggleServiceStatus, getAllServices} from "../../controller/admin/adminServiceController.js";
+import { createService ,updateService,deleteService,toggleServiceStatus, getAllServices, getServiceById} from "../../controller/admin/adminServiceController.js";
 import { protectAdmin } from "../../middleware/adminauthMiddleware.js";
 
 const router = express.Router();
 router.get("/",protectAdmin,getAllServices)
-router.get("/:id",protectAdmin,getAllServices)
+router.get("/:id",protectAdmin,getServiceById)
 router.post("/", protectAdmin, createService);
 router.put("/:id", protectAdmin, updateService);
 router.delete("/:id", protectAdmin, deleteService);
