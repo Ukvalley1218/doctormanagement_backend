d77dce8c10b90581ccb97d4e0478e685378f5e5c
tejas kahirnar <tejaskhairnar256@gmail.com>
2026-04-29 17:52:10 +0530

    first commit

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const allowedOrigins = [
   "https://www.healcure.ca",
   "https://healcure.valleyhoster.com",
   "http://localhost:5174",
-  "https://doctor-management-admin.vercel.app"
+  "https://doctor-management-admin.vercel.app","https://doctormanagemend.vercel.app"
 ];
 
 app.use(
